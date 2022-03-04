380e02f2ffd09262cf4f62b5be27263dfccb26b2
Joao Victor <jov9051@outlook.com>
2022-03-04 14:48:01 -0300

    var env DB_PORT adicionada a .env

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,3 +1,12 @@
 import sequelize from 'sequelize';
 
 let { Sequelize } = sequelize;
+
+// const dbConnection = new Sequelize(
+//     process.env.DB_DATABASE,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST
+//     }
+// );
