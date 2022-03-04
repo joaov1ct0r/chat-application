4dd07bd2b154b34f5f1b710ab6d345bf2d63345a
Joao Victor <jov9051@outlook.com>
2022-03-04 14:49:29 -0300

    dbConnection criada em userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -2,11 +2,13 @@ import sequelize from 'sequelize';
 
 let { Sequelize } = sequelize;
 
-// const dbConnection = new Sequelize(
-//     process.env.DB_DATABASE,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//         host: process.env.DB_HOST
-//     }
-// );
+const dbConnection = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql'
+    }
+);
