deb2c24762221cf533bed297a2b083cc6d8eeac1
Joao Victor <jov9051@outlook.com>
2022-03-04 14:32:33 -0300

    var env DB_HOST adicionada a .evn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import express from 'express';
 
 import mysql from 'mysql2';
 
+// let dbConnection = mysql.createConnection({})
+
 let app = express();
 
 app.listen(process.env.SERVER_PORT, () => {
