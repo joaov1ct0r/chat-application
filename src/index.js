8e61b7af53a083c068cd38fc96c24cf41d492e68
Joao Victor <jov9051@outlook.com>
2022-03-04 15:07:07 -0300

    conexao com mysql estabelecida

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ let { Sequelize } = sequelize;
 
 let app = express();
 
+const dbConnection = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+});
+
+try {
+    await dbConnection.authenticate();
+
+    console.log('Conexão estabelecida com sucesso!');
+} catch (error) {
+    throw error;
+}
+
 app.listen(process.env.SERVER_PORT, () => {
     console.log('Server running');
 });
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,22 +1,3 @@
 import sequelize from 'sequelize';
 
 let { Sequelize } = sequelize;
-
-const dbConnection = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
-});
-
-try {
-    let connected = await dbConnection.authenticate();
-
-    if (connected) {
-        console.log('Conexão estabelecida com sucesso!');
-    }
-} catch (error) {
-    throw error;
-}
