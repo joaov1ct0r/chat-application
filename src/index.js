57019902066a8f6949c436829630cba344318e75
Joao Victor <jov9051@outlook.com>
2022-03-04 15:05:30 -0300

    sequelize declarado em index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import 'dotenv/config';
 
 import express from 'express';
 
+import sequelize from 'sequelize';
+
+let { Sequelize } = sequelize;
+
 let app = express();
 
 app.listen(process.env.SERVER_PORT, () => {
