2753d7826dd752902e1f96391757bddd7413d072
Joao Victor <jov9051@outlook.com>
2022-03-04 15:36:48 -0300

    user sendo exportado de userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -2,27 +2,33 @@ import { DataTypes } from 'sequelize';
 
 import dbConnection from '../config/database/database';
 
-const user = dbConnection.define('user', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
+const user = dbConnection.define(
+    'users',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nascimento: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        senha: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    nome: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    nascimento: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    senha: {
-        type: DataTypes.STRING,
-        allowNull: false
+    {
+        freezeTableName: true
     }
-});
+);
 
 export default user;
