10fdac6bd859c86b822b80c934f979f5bd1174bd
Joao Victor <jov9051@outlook.com>
2022-03-04 14:36:57 -0300

    codigo inutilizado removido

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import express from 'express';
 
 import mysql from 'mysql2';
 
-let dbConnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
-
 let app = express();
 
 app.listen(process.env.SERVER_PORT, () => {
