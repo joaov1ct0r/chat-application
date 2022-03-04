f7b75351629ecd97a4e454275c289b8f1866e075
Joao Victor <jov9051@outlook.com>
2022-03-04 15:34:10 -0300

    userModel.js criado na pasta models

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,3 +1,26 @@
-import sequelize from 'sequelize';
+import { DataTypes } from 'sequelize';
 
 import dbConnection from '../config/database/database';
+
+const user = dbConnection.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nascimento: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
