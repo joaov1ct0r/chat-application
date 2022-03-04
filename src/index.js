1b64fda4be6de9355e15cdaa506cfcf42c0186fe
Joao Victor <jov9051@outlook.com>
2022-03-04 14:35:16 -0300

    dbConnection criada em index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import express from 'express';
 
 import mysql from 'mysql2';
 
-// let dbConnection = mysql.createConnection({})
+let dbConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
 
 let app = express();
 
