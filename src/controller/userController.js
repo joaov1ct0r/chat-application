b12d8f1f9ddfc97c6cf9be69fab4708d65050217
Joao Victor <jov9051@outlook.com>
2022-03-05 14:36:04 -0300

    obj user recebendo dados de novo usuario

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,11 +2,19 @@ import User from '../model/userModel';
 
 let user = {
     createUser(req, res) {
-        let user = new User({
+        let user = User.build({
             email: req.body.email,
             nome: req.body.nome,
             nascimento: req.body.nascimento,
             senha: req.body.senha
         });
+
+        try {
+            const savedUser = await user.save();
+
+            res.send(savedUser);
+        } catch (error) {
+            res.status(400).send(error);
+        }
     }
 };
