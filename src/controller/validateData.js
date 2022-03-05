77534e591b4f2db6bc91e8c4ebb3d2a0f93d2b86
Joao Victor <jov9051@outlook.com>
2022-03-05 17:33:23 -0300

    registerValidate schema criado em registerValidate

--- src/controller/validateData.js
+++ src/controller/validateData.js
@@ -1,1 +1,10 @@
 import Joi from '@hapi/joi';
+
+const registerValidate = data => {
+    const schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        nome: Joi.string().required().min(10).max(100),
+        nascimento: Joi.string().required().min(10).max(10),
+        senha: Joi.string().required().min(8).max(250)
+    });
+};
