e3555c133fbb1bb36d5cb4a47cc8df60580913ca
Joao Victor <jov9051@outlook.com>
2022-03-07 16:21:13 -0300

    url declarado em index.js

--- src/view/registro/scripts/script.js
+++ src/view/registro/scripts/script.js
@@ -3,3 +3,7 @@ let submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', () => {
     userRegister();
 });
+
+function userRegister() {
+    let url = 'http://localhost:3000/api/register';
+}
