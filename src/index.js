4f443fc96a3e265eb3fc042619644d257462eb6c
Joao Victor <jov9051@outlook.com>
2022-03-07 11:16:11 -0300

    dirname declarado em index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 
+const __dirname = path.dirname(__filename);
+
 let app = express();
 
 app.use('/api', express.json(), router);
