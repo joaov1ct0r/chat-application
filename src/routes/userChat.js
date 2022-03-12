b3a1a213a60494844391229a05422a194b1bd2de
Joao Victor <jov9051@outlook.com>
2022-03-12 13:13:57 -0300

    dirname declarado userChat.js

--- src/routes/userChat.js
+++ src/routes/userChat.js
@@ -8,6 +8,8 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 
+const __dirname = path.dirname(__filename);
+
 let router = express.Router();
 
 export default router;
