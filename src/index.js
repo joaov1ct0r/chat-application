05f9fcda49ecf66a78d8f3b9d6e6268fd9ea6fb1
Joao Victor <jov9051@outlook.com>
2022-03-12 13:17:29 -0300

    routerChat importado para index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 
+import routerChat from './routes/userChatRoutes.js';
+
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
