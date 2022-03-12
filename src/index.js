221116ab4ace239b0e72ef1716e9c2058ba16994
Joao Victor <jov9051@outlook.com>
2022-03-12 13:12:24 -0300

    codigo inutilizado removido

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 
-import authController from './controller/authController.js';
-
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
