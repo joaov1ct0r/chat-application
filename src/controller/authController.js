2a46044357d47c347321fcf912368a72457bd8b1
Joao Victor <jov9051@outlook.com>
2022-03-12 12:25:44 -0300

    condicional checando token criada

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -2,4 +2,8 @@ import jwt from 'jsonwebtoken';
 
 export default function (req, res, next) {
     const token = req.header('auth-token');
+
+    if (!token) {
+        return res.status(401).send('Falha na autenticação');
+    }
 }
