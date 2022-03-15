718cf40ceda826fb021e21d5dceb6baa2a3eae82
Joao Victor <jov9051@outlook.com>
2022-03-15 13:33:19 -0300

    codigo inutilizado removido

--- src/controller/chatController.js
+++
@@ -1,9 +0,0 @@
-import io from '../index.js';
-
-let onConnection = socket => {
-    console.log('new connection');
-};
-
-const connectionHandle = io.on('connection', onConnection);
-
-export default connectionHandle;
