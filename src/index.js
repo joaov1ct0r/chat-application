ab3038164c8d6582a6daeceb0bc7010659e7b7d4
Joao Victor <jov9051@outlook.com>
2022-03-16 16:15:45 -0300

    enviando msg ao socket

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const io = new Server(server);
 io.on('connection', socket => {
     console.log('new connection');
 
-    io.emit('new connection', { msg: 'Novo usuario conectado' });
+    socket.broadcast.emit('new connection', { msg: 'Novo usuario conectado' });
 
     socket.emit('welcome', { msg: 'Seja bem vindo!' });
 });
