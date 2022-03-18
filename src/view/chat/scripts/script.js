e4bb75fd563fe23dc2b680f053e91003428c3b8e
Joao Victor <jov9051@outlook.com>
2022-03-18 17:07:18 -0300

    handleNewConnection criada em script.js

--- src/view/chat/scripts/script.js
+++ src/view/chat/scripts/script.js
@@ -1,5 +1,13 @@
+const ul = document.getElementsByTagName('ul')[0];
+
 const socket = io('http://localhost:3000');
 
 socket.on('new connection', data => {
-    console.log(data.msg);
+    handleNewConnection(data);
 });
+
+function handleNewConnection({ msg }) {
+    const newMessage = `<li>${msg}</li>`;
+
+    ul.innerHTML += newMessage;
+}
