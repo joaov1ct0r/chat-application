83480fb27bbabbdc465e8c0e8470dd03a59ab0b8
Joao Victor <jov9051@outlook.com>
2022-03-22 18:52:44 -0300

    new_message criado em handleConnection.js

--- src/controller/handleConnection.js
+++ src/controller/handleConnection.js
@@ -4,6 +4,10 @@ function handleConnection(socket) {
     socket.broadcast.emit('new connection', { msg: 'Novo usuario conectado' });
 
     socket.emit('welcome', { msg: 'Seja bem vindo!' });
+
+    socket.on('new_message', data => {
+        messages.push(data);
+    });
 }
 
 export default handleConnection;
