ab2c5a3578f162056e611be81b7bb65c8c6185f4
Joao Victor <jov9051@outlook.com>
2022-03-22 19:07:58 -0300

    submitButton esperando evento

--- src/view/chat/scripts/script.js
+++ src/view/chat/scripts/script.js
@@ -2,6 +2,10 @@ const ul = document.getElementsByTagName('ul')[0];
 
 const submitButton = document.getElementById('submitButton');
 
+submitButton.addEventListener('click', () => {
+    sendNewMessage();
+});
+
 const socket = io('http://localhost:3000');
 
 socket.on('new connection', data => {
