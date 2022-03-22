f1847305f1eedaeac5534d64ae6e1ddc7e1d1749
Joao Victor <jov9051@outlook.com>
2022-03-22 20:13:18 -0300

    messages sendo enviadas a novas conexoes

--- src/controller/handleIo.js
+++ src/controller/handleIo.js
@@ -12,6 +12,8 @@ export default function (server) {
 
         socket.emit('welcome', { msg: 'Seja bem vindo!' });
 
+        socket.emit('messages', messages);
+
         socket.on('new_message', data => {
             messages.push(data.msg);
 
