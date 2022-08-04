8719542e87661419fd14217f99a83b6a5d7681be
Joao Victor <jov9051@outlook.com>
2022-08-04 17:05:07 -0300

    refactor: validateUserData.ts tipada

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,85 +1,88 @@
-import User from '../model/userModel.js';
+import User from "../model/userModel.js";
 
-import { loginValidate, registerValidate } from './validateData.js';
+import {
+  loginValidate,
+  registerValidate,
+} from "../validations/validateUserData.js";
 
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const createUser = async (req, res) => {
-    let { error } = registerValidate(req.body);
+  const { error } = registerValidate(req.body);
 
-    if (error) {
-        return res.status(400).send(error);
+  if (error) {
+    return res.status(400).send(error);
+  }
+
+  const registeredUser = await User.findOne({
+    where: { email: req.body.email },
+  });
+
+  if (registeredUser) {
+    return res.status(400).send("Email ja cadastrado");
+  }
+
+  const user = User.build({
+    email: req.body.email,
+    nome: req.body.nome,
+    nascimento: req.body.nascimento,
+    senha: bcrypt.hashSync(req.body.senha),
+  });
+
+  try {
+    const savedUser = await user.save();
+
+    if (savedUser) {
+      res.redirect("/");
+    } else {
+      res.redirect("/register");
     }
-
-    let registeredUser = await User.findOne({
-        where: { email: req.body.email }
-    });
-
-    if (registeredUser) {
-        return res.status(400).send('Email ja cadastrado');
-    }
-
-    let user = User.build({
-        email: req.body.email,
-        nome: req.body.nome,
-        nascimento: req.body.nascimento,
-        senha: bcrypt.hashSync(req.body.senha)
-    });
-
-    try {
-        const savedUser = await user.save();
-
-        if (savedUser) {
-            res.redirect('/');
-        } else {
-            res.redirect('/register');
-        }
-    } catch (error) {
-        res.status(400).send(error);
-    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 const userLogin = async (req, res) => {
-    let { error } = loginValidate(req.body);
+  const { error } = loginValidate(req.body);
 
-    if (error) {
-        return res.status(400).send(error);
-    }
-    let selectedUser = await User.findOne({
-        where: { email: req.body.email }
-    });
+  if (error) {
+    return res.status(400).send(error);
+  }
+  const selectedUser = await User.findOne({
+    where: { email: req.body.email },
+  });
 
-    if (!selectedUser) {
-        return res.status(400).send('Falha na autenticação');
-    }
+  if (!selectedUser) {
+    return res.status(400).send("Falha na autenticação");
+  }
 
-    let comparedPassword = bcrypt.compareSync(
-        req.body.senha,
-        selectedUser.senha
+  const comparedPassword = bcrypt.compareSync(
+    req.body.senha,
+    selectedUser.senha
+  );
+
+  if (!comparedPassword) {
+    return res.status(400).send("Falha na autenticação");
+  }
+
+  try {
+    const token = jwt.sign(
+      {
+        id: selectedUser.id,
+      },
+      process.env.JWT_TOKEN_SECRET
     );
 
-    if (!comparedPassword) {
-        return res.status(400).send('Falha na autenticação');
+    if (token) {
+      res.cookie("auth", token, { httpOnly: true });
+
+      res.redirect("/chat");
     }
-
-    try {
-        let token = jwt.sign(
-            {
-                id: selectedUser.id
-            },
-            process.env.JWT_TOKEN_SECRET
-        );
-
-        if (token) {
-            res.cookie('auth', token, { httpOnly: true });
-
-            res.redirect('/chat');
-        }
-    } catch (error) {
-        throw error;
-    }
+  } catch (error) {
+    throw error;
+  }
 };
 
 export { createUser, userLogin };
---
+++ src/validations/validateUserData.ts
@@ -0,0 +1,23 @@
+import Joi from "@hapi/joi";
+
+const registerValidate = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    email: Joi.string().required().min(10).max(100),
+    nome: Joi.string().required().min(10).max(100),
+    nascimento: Joi.string().required().min(10).max(10),
+    senha: Joi.string().required().min(8).max(250),
+  });
+
+  return schema.validate(data);
+};
+
+const loginValidate = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    email: Joi.string().required().min(10).max(100),
+    senha: Joi.string().required().min(8).max(250),
+  });
+
+  return schema.validate(data);
+};
+
+export { loginValidate, registerValidate };
