e8296e62e3ad58fbb50b0c1103c178c496a20172
Joao Victor <jov9051@outlook.com>
2022-08-04 13:01:01 -0300

    feat: config.js

---
+++ config/config.js
@@ -0,0 +1,27 @@
+import "dotenv/config";
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: true,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
