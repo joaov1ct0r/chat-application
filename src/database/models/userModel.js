d8666deb29e20e32efeb42145527dba11354f961
Joao Victor <jov9051@outlook.com>
2022-08-04 13:07:37 -0300

    feat: userModel.js

---
+++ src/database/models/userModel.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+
+import dbConnection from "../config/database/database.js";
+
+const User = dbConnection.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    nome: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    nascimento: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    senha: {
+      type: DataTypes.STRING(250),
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+User.sync();
+
+export default User;
