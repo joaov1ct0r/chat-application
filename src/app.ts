d3e58c2ccf30c11f54697ee4e03fa26b796aefe6
Joao Victor <jov9051@outlook.com>
2022-08-04 17:00:29 -0300

    feat: app.ts criado em /src

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,13 @@ import cookieParser from "cookie-parser";
 
 import cors from "cors";
 
+import auth from "./middlewares/auth";
+
 import userRouter from "./routes/userRoutes.js";
+
+import swaggerUi from "swagger-ui-express";
+
+import swaggerDocs from "./swagger.json";
 
 import socketIO from "./controller/handleIo.js";
 
@@ -19,31 +25,49 @@ const __dirname = path.dirname(__filename);
 export default class App {
   public server: express.Application;
 
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+
+    this.userRoutes();
+
+    this.docsRoutes();
+  }
+
   private middlewares() {
+    this.server.use(cors());
+
+    this.server.use(cookieParser());
+
     this.server.use(express.json());
+
+    this.server.use(express.urlencoded({ extended: true }));
+
+    this.server.use(
+      "/",
+      express.static(path.join(__dirname, "/view", "/login"))
+    );
+
+    this.server.use(
+      "/register",
+      express.static(path.join(__dirname, "/view", "/registro"))
+    );
+
+    app.use(
+      "/chat",
+      auth,
+      express.static(path.join(__dirname, "/view", "/chat"))
+    );
+  }
+
+  private userRoutes() {
+    this.server.use("/api/user", userRouter);
+  }
+
+  private docsRoutes() {
+    this.server.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
   }
 }
-
-const app = express();
-
-app.use("/api", router);
-
-app.use("/", express.static(path.join(__dirname, "/view", "/login")));
-
-app.use(
-  "/register",
-  express.static(path.join(__dirname, "/view", "/registro"))
-);
-
-app.use(
-  "/chat",
-  cookieParser(),
-  authController,
-  express.static(path.join(__dirname, "/view", "/chat"))
-);
-
-const server = app.listen(process.env.SERVER_PORT, () => {
-  console.log("Server running");
-});
 
 handleIo(server);
