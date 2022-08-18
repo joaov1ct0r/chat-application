ed4f401eae5e8748172834b60c62311943ba9946
Joao Victor <jov9051@outlook.com>
2022-08-18 11:41:58 -0300

    feat: func getRedis criado em redisConfig.ts

--- src/utils/redisConfig.ts
+++ src/utils/redisConfig.ts
@@ -1,5 +1,13 @@
 import Redis from "ioredis";
 
+import { promisify } from "util";
+
 const redisClient = new Redis();
 
-export { redisClient };
+const getRedis = (value: string) => {
+  const syncRedisGet = promisify(redisClient.get).bind(redisClient);
+
+  return syncRedisGet(value);
+};
+
+export { redisClient, getRedis };
