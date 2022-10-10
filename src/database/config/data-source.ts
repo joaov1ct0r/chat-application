1b524d7a843600dedc51ca2649a779a68b15f98d
Joao Victor <jov9051@outlook.com>
2022-10-10 20:27:00 -0300

    feat: data-source.ts criado em config

---
+++ src/database/config/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from "typeorm";
+
+import User from "../entity/userModel";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database:
+    process.env.NODE_ENV === "production"
+      ? process.env.DB_DATABASE
+      : process.env.DB_DATABASE_TEST,
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  subscribers: [],
+  migrations: [],
+});
