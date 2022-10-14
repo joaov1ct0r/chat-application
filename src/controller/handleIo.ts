4e92e5ce6293073075b14eb188af7ea63b8704f7
Joao Victor <jov9051@outlook.com>
2022-10-14 17:25:31 -0300

    feat: getRedis, setRedis importados para handleIo.ts

--- src/controller/handleIo.ts
+++ src/controller/handleIo.ts
@@ -4,24 +4,24 @@ import { getRedis, setRedis } from "../utils/redisConfig";
 
 import IDataIO from "../interfaces/IDataIO";
 
-export default function socketIO(server) {
+export default function socketIO(server: any) {
   const io = new Server(server);
 
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     socket.broadcast.emit("new connection", {
       msg: "Novo usuario conectado",
     });
 
     socket.emit("welcome", { msg: "Seja bem vindo!" });
 
-    socket.emit("messages", JSON.parse(getRedis("all")));
+    socket.emit("messages", JSON.parse(String(getRedis("all"))));
 
     socket.on("new_message", (data: IDataIO) => {
       setRedis("all", JSON.stringify(data));
 
       setRedis(`${data.user}`, data.msg);
 
-      io.emit("messages", JSON.parse(getRedis("all")));
+      io.emit("messages", JSON.parse(String(getRedis("all"))));
     });
   });
 }
