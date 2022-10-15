1eab46edf1385b0b95f0c6d13b6252eab67a52bc
Joao Victor <jov9051@outlook.com>
2022-10-15 17:36:27 -0300

    feat: metodos findOneBy e create adicionados a CreateUserService.ts

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,32 +1,40 @@
-import User from "../database/entities/User";
-
 import IUser from "../interfaces/IUser";
 
 import bcrypt from "bcryptjs";
 
 import BadRequestError from "../errors/BadRequestError";
 
+import { Repository } from "typeorm";
+
 export default class CreateUserService {
+  private readonly repository: Repository<IUser>;
+
+  constructor(repository: Repository<IUser>) {
+    this.repository = repository;
+  }
+
   public async execute(
     email: string,
     nome: string,
     nascimento: string,
     senha: string
   ): Promise<IUser> {
-    const user: IUser | null = await User.findOne({
-      where: { email },
+    const user: IUser | null = await this.repository.findOneBy({
+      email,
     });
 
     if (user === null) {
       throw new BadRequestError("Email ja cadastrado");
     }
 
-    const newUser: IUser = await User.create({
+    const newUser: IUser = this.repository.create({
       email,
       nome,
       nascimento,
       senha: bcrypt.hashSync(senha),
     });
+
+    await this.repository.save(newUser);
 
     return newUser;
   }
