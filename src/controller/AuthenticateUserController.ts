e52be03e59fdf1a474f647971215358784334653
Joao Victor <jov9051@outlook.com>
2022-10-15 17:52:36 -0300

    feat: repository pattern adicionado a AuthenticateUserController.ts

--- src/controller/AuthenticateUserController.ts
+++ src/controller/AuthenticateUserController.ts
@@ -6,6 +6,14 @@ import AuthenticateUserService from "../services/AuthenticateUserService";
 
 import IAuthenticateUserService from "../interfaces/IAuthenticateUserService";
 
+import DB from "../database/config/data-source";
+
+import { Repository } from "typeorm";
+
+import User from "../database/entities/User";
+
+import IUser from "../interfaces/IUser";
+
 export default class AuthenticateUserController {
   public async handle(req: Request, res: Response): Promise<void | Response> {
     const { error } = loginValidate(req.body);
@@ -18,8 +26,10 @@ export default class AuthenticateUserController {
 
     const senha: string = req.body.senha;
 
+    const repository: Repository<IUser> = DB.getRepository(User);
+
     const authenticateUserService: IAuthenticateUserService =
-      new AuthenticateUserService();
+      new AuthenticateUserService(repository);
 
     try {
       const token: string = await authenticateUserService.execute(email, senha);
