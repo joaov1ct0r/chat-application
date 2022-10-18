d8b2772f555d13e393e7aa4885ad88538f9e16f4
Joao Victor <jov9051@outlook.com>
2022-10-18 19:43:15 -0300

    feat: views adicionado a volumes em docker-compose.yaml

--- src/app.ts
+++ src/app.ts
@@ -38,27 +38,11 @@ export default class App {
   private userRoutes() {
     this.server.use("/api/user", userRouter);
 
-    this.server.use(
-      "/",
-      express.static(
-        "/home/joaovictor/Me/projetos/finishedProjects/node-chat-socket-mvc/src/views/login"
-      )
-    );
+    this.server.use("/", express.static("./views/login"));
 
-    this.server.use(
-      "/register",
-      express.static(
-        "/home/joaovictor/Me/projetos/finishedProjects/node-chat-socket-mvc/src/views/registro"
-      )
-    );
+    this.server.use("/register", express.static("./views/registro"));
 
-    this.server.use(
-      "/chat",
-      auth,
-      express.static(
-        "/home/joaovictor/Me/projetos/finishedProjects/node-chat-socket-mvc/src/views/chat"
-      )
-    );
+    this.server.use("/chat", auth, express.static("./views/chat"));
   }
 
   private docsRoutes() {
