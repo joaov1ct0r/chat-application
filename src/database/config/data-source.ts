c8ae2dd475f6d825fbb1b0d8e9a4085b6c02fc2b
Joao Victor <jov9051@outlook.com>
2022-10-20 17:35:05 -0300

    feat: path para dir de redis e mysql dev criados

--- src/database/config/data-source.ts
+++ src/database/config/data-source.ts
@@ -6,7 +6,7 @@ const DB: DataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
+  username: "root" as string,
   password: process.env.DB_PASSWORD,
   database:
     process.env.NODE_ENV === "production"
