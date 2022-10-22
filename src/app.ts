7231827613487d5078df52bd5979a505d7a203d9
Joao Victor <jov9051@outlook.com>
2022-10-22 15:02:45 -0300

    feat: cors sendo utilizado pelo servidor

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,16 @@ import swaggerUi from "swagger-ui-express";
 
 import swaggerDocs from "./swagger.json";
 
+import ICors from "./interfaces/ICors";
+
 export default class App {
   public server: express.Application;
+
+  private readonly corsOptions: ICors = {
+    credentials: true,
+
+    origin: ["http://localhost:3001", "http://localhost:3000"],
+  };
 
   constructor() {
     this.server = express();
@@ -26,7 +34,7 @@ export default class App {
   }
 
   private middlewares() {
-    this.server.use(cors());
+    this.server.use(cors(this.corsOptions));
 
     this.server.use(cookieParser());
 
---
+++ src/interfaces/ICors.ts
@@ -0,0 +1,5 @@
+export default interface ICors {
+  origin: string[];
+
+  credentials: boolean;
+}
