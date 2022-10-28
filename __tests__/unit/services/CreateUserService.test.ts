ae885d10aced4527138a4ba12dadd29b6b1f60d7
Joao Victor <jov9051@outlook.com>
2022-10-28 19:11:19 -0300

    test: teste para retorno de exception quando user ja for registrado, criado

---
+++ __tests__/unit/services/CreateUserService.test.ts
@@ -0,0 +1,40 @@
+import { mock } from "jest-mock-extended";
+
+import IUser from "../../../src/interfaces/IUser";
+
+import CreateUserService from "../../../src/services/CreateUserService";
+
+import ICreateUserService from "../../../src/interfaces/ICreateUserService";
+
+import { Repository } from "typeorm";
+
+import BadRequestError from "../../../src/errors/BadRequestError";
+
+const makeSut = () => {
+  const mockRepository = mock<Repository<IUser>>();
+
+  const sut: ICreateUserService = new CreateUserService(mockRepository);
+
+  return { mockRepository, sut };
+};
+
+describe("create user service", () => {
+  describe("when execute is called", () => {
+    it("should throw an error if user already exists", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      mockRepository.findOneBy.mockResolvedValueOnce({
+        id: "1",
+      } as unknown as IUser);
+
+      expect(async () => {
+        await sut.execute(
+          "user1234@mail.com.br",
+          "user nome",
+          "54385092-394-29-2",
+          "00/00/0000"
+        );
+      }).rejects.toThrow(new BadRequestError("User ja cadastrado"));
+    });
+  });
+});
