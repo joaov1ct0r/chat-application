6f3f526e0a97a3c912c4c1de6f0980b3ce114a08
Joao Victor <jov9051@outlook.com>
2023-01-06 20:37:48 -0300

    refactor: userRoutes refatorado

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,24 +1,23 @@
 import express from "express";
-
-import resolver from "../utils/Resolver";
-
+import Resolver from "../utils/Resolver";
 import CreateUserController from "../controller/CreateUserController";
-
-import ICreateUserController from "../interfaces/ICreateUserController";
-
 import AuthenticateUserController from "../controller/AuthenticateUserController";
-
-import IAuthenticateUserController from "../interfaces/IAuthenticateUserController";
 
 const userRouter: express.Router = express.Router();
 
-const createUserController: ICreateUserController = new CreateUserController();
+const createUserController: CreateUserController = new CreateUserController();
 
-const authenticateUserController: IAuthenticateUserController =
+const authenticateUserController: AuthenticateUserController =
   new AuthenticateUserController();
 
-userRouter.post("/login", resolver(authenticateUserController.handle));
+userRouter.post(
+  "/login",
+  new Resolver().handle(authenticateUserController.handle)
+);
 
-userRouter.post("/register", resolver(createUserController.handle));
+userRouter.post(
+  "/register",
+  new Resolver().handle(createUserController.handle)
+);
 
 export default userRouter;
