895471459cc701ed4b5fbc59751dbc8c87ffd2f8
Joao Victor <jov9051@outlook.com>
2023-01-06 20:25:17 -0300

    refactor: Resolver refatorado para class

--- src/utils/Resolver.ts
+++ src/utils/Resolver.ts
@@ -1,9 +1,11 @@
 import { Handler, NextFunction, Request, Response } from "express";
 
-const resolver = (handlerFn: Handler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    return Promise.resolve(handlerFn(req, res, next)).catch((err) => next(err));
-  };
-};
-
-export default resolver;
+export default class Resolver {
+  handle(handlerFn: Handler) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      return Promise.resolve(handlerFn(req, res, next)).catch((err) =>
+        next(err)
+      );
+    };
+  }
+}
