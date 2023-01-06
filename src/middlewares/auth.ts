37a1879c0c70cd3bef94ac380a3f4ba1c7afb76f
Joao Victor <jov9051@outlook.com>
2023-01-06 20:36:09 -0300

    refactor: auth refatorado e errors adicionados

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,31 +1,43 @@
 import { Response, NextFunction } from "express";
-
 import IReq from "../interfaces/IRequest";
-
 import jwt from "jsonwebtoken";
-
 import IJwt from "../interfaces/IJwt";
+import BadRequestError from "../errors/BadRequestError";
+import ForbiddenError from "../errors/ForbiddenError";
+import InternalError from "../errors/InternalError";
 
-export default function (req: IReq, res: Response, next: NextFunction) {
-  const token: string = req.cookies.authorization.split(" ")[1];
+export default class Authorization {
+  handle(req: IReq, res: Response, next: NextFunction) {
+    const token: string = req.cookies.authorization.split(" ")[1];
 
-  if (token.length === 0) {
-    return res.status(400).json({ error: "Token não encontrado!" });
-  }
+    if (token.length === 0) {
+      const error = new BadRequestError("Token não encontrado!");
 
-  try {
-    const verifiedToken: IJwt = jwt.verify(
-      token,
-      process.env.JWT_TOKEN_SECRET as string
-    ) as IJwt;
-
-    if (!verifiedToken) {
-      return res.status(403).json({ error: "Falha na autenticação!" });
+      return res
+        .status(error.statusCode)
+        .json({ error: error.message, status: error.statusCode });
     }
-    req.userId = verifiedToken.id;
 
-    next();
-  } catch (err: unknown) {
-    return res.status(500).json({ err });
+    try {
+      const verifiedToken: IJwt = jwt.verify(
+        token,
+        process.env.JWT_TOKEN_SECRET as string
+      ) as IJwt;
+
+      if (!verifiedToken) {
+        const error = new ForbiddenError("Falha na autenticação!");
+        return res
+          .status(error.statusCode)
+          .json({ error: error.message, status: error.statusCode });
+      }
+      req.userId = verifiedToken.id;
+
+      next();
+    } catch (err: unknown) {
+      const error = new InternalError("Erro Interno");
+      return res
+        .status(error.statusCode)
+        .json({ error: error.message, status: error.statusCode });
+    }
   }
 }
