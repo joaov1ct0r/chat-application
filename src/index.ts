8d03d2adeae10890a63fda43f53802b21a5e61f3
Joao Victor <jov9051@outlook.com>
2023-01-06 20:17:53 -0300

    refactor: tipagem de erros adicionado a index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import "reflect-metadata";
-
 import "dotenv/config";
-
 import App from "./app";
-
 import socketIO from "./controller/handleIo";
-
 import DB from "./database/config/data-source";
 
 const server = new App().server.listen(
   Number(process.env.SERVER_PORT!),
   String(process.env.SERVER_HOST!),
   async () => {
-    let retries: number = 5;
+    let retries = 5;
 
     while (retries) {
       try {
@@ -23,7 +20,7 @@ const server = new App().server.listen(
         console.log("Server running");
 
         break;
-      } catch (error: any) {
+      } catch (error: unknown) {
         console.log(error);
 
         retries -= 1;
