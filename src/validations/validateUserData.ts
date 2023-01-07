40f69623e734ef90ae43d5dd0ab695b96c3289f6
Joao Victor <jov9051@outlook.com>
2023-01-07 19:24:08 -0300

    refactor: validator ValidateUser criado

--- src/validations/validateUserData.ts
+++ src/validations/validateUserData.ts
@@ -1,23 +1,23 @@
 import Joi from "@hapi/joi";
 
-const registerValidate = (data: object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<object> = Joi.object({
-    email: Joi.string().required().min(10).max(100),
-    name: Joi.string().required().min(10).max(100),
-    nascimento: Joi.string().required().min(10).max(10),
-    senha: Joi.string().required().min(8).max(250),
-  });
+export default class ValidateUser {
+  registerValidate(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<object> = Joi.object({
+      email: Joi.string().required().min(10).max(100),
+      name: Joi.string().required().min(10).max(100),
+      nascimento: Joi.string().required().min(10).max(10),
+      senha: Joi.string().required().min(8).max(250),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const loginValidate = (data: object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<object> = Joi.object({
-    email: Joi.string().required().min(10).max(100),
-    senha: Joi.string().required().min(8).max(250),
-  });
+  loginValidate(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<object> = Joi.object({
+      email: Joi.string().required().min(10).max(100),
+      senha: Joi.string().required().min(8).max(250),
+    });
 
-  return schema.validate(data);
-};
-
-export { loginValidate, registerValidate };
+    return schema.validate(data);
+  }
+}
