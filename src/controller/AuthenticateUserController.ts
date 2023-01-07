086cdf8310c85a61bbe4206838bb762a9dfd9c11
Joao Victor <jov9051@outlook.com>
2023-01-06 22:05:53 -0300

    refactor: AuthenticateUserRepository importado para controller

--- src/controller/AuthenticateUserController.ts
+++ src/controller/AuthenticateUserController.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from "express";
 import { loginValidate } from "../validations/validateUserData";
 import AuthenticateUserService from "../services/AuthenticateUserService";
-import DB from "../database/config/data-source";
-import { Repository } from "typeorm";
-import User from "../database/entities/User";
-import IUser from "../interfaces/IUser";
 import BadRequestError from "../errors/BadRequestError";
+import AuthenticateUserRepository from "../database/repositories/AuthenticateUserRepository";
 
 export default class AuthenticateUserController {
   public async handle(req: Request, res: Response): Promise<void | Response> {
@@ -21,9 +18,10 @@ export default class AuthenticateUserController {
 
     const senha: string = req.body.senha;
 
-    const repository: Repository<IUser> = DB.getRepository(User);
+    const repository: AuthenticateUserRepository =
+      new AuthenticateUserRepository();
 
-    const authenticateUserService: IAuthenticateUserService =
+    const authenticateUserService: AuthenticateUserService =
       new AuthenticateUserService(repository);
 
     try {
