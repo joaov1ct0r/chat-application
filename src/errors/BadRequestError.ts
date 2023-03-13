4fa86435a4e56a95241dd2139342166f7de90524
joaov1ct0r <jov9051@outlook.com>
2023-03-13 19:53:28 -0300

    refactor: BadReqeustError

--- src/errors/BadRequestError.ts
+++ src/errors/BadRequestError.ts
@@ -1,6 +1,5 @@
 export default class BadRequestError extends Error {
   public readonly message: string;
-
   public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
