3f71331cc84aea8f59e0ac0da9d68a3f80574337
joaov1ct0r <jov9051@outlook.com>
2023-04-24 21:58:08 -0300

    refactor: validators

--- src/validations/validateUserData.ts
+++ src/validations/validateUserData.ts
@@ -1,25 +1,25 @@
-import Joi from "@hapi/joi";
+import Joi from '@hapi/joi'
 
 export default class ValidateUser {
-  private schema: Joi.ObjectSchema<object> | undefined;
+  private schema: Joi.ObjectSchema<object> | undefined
 
-  registerValidate(data: object): Joi.ValidationResult {
+  registerValidate (data: object): Joi.ValidationResult {
     this.schema = Joi.object({
       email: Joi.string().required().min(10).max(100),
       name: Joi.string().required().min(10).max(100),
       nascimento: Joi.string().required().min(10).max(10),
-      senha: Joi.string().required().min(8).max(250),
-    });
+      senha: Joi.string().required().min(8).max(250)
+    })
 
-    return this.schema.validate(data);
+    return this.schema.validate(data)
   }
 
-  loginValidate(data: object): Joi.ValidationResult {
+  loginValidate (data: object): Joi.ValidationResult {
     this.schema = Joi.object({
       email: Joi.string().required().min(10).max(100),
-      senha: Joi.string().required().min(8).max(250),
-    });
+      senha: Joi.string().required().min(8).max(250)
+    })
 
-    return this.schema.validate(data);
+    return this.schema.validate(data)
   }
 }
