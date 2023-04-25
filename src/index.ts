0c619ddd917c6d9c9d15a1a21a36e81c3bf1cda1
joaov1ct0r <jov9051@outlook.com>
2023-04-25 20:20:05 -0300

    refactor: index

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,15 @@ import 'reflect-metadata'
 import 'dotenv/config'
 import App from './app'
 import SocketIO from './controller/handleIo'
-import DB from './database/config/data-source'
+import ConnectDB from './utils/connectDb'
 
-const connect = async (): Promise<void> => {
-  let retries = 5
-
-  while (retries > 0) {
-    try {
-      await DB.initialize()
-
-      console.log('DB Connected')
-
-      console.log('Server running')
-
-      break
-    } catch (error) {
-      console.log(error)
-
-      retries -= 1
-
-      console.log(`retries left ${retries}!`)
-
-      await new Promise((resolve) => setTimeout(resolve, 5000))
-    }
-  }
-}
+const db = new ConnectDB()
 
 const server = new App().server.listen(
   Number(process.env.SERVER_PORT),
   String(process.env.SERVER_HOST),
   () => {
-    connect().catch((error) => {
+    db.execute().catch((error) => {
       console.log(error)
       console.log('Não foi possível se conectar ao DB')
     })
