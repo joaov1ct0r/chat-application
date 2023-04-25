0979985c244ccd71760ce0e66260f34d855fff7c
joaov1ct0r <jov9051@outlook.com>
2023-04-25 20:35:28 -0300

    refactor: Resolver

--- src/utils/Resolver.ts
+++ src/utils/Resolver.ts
@@ -1,11 +1,11 @@
-import { Handler, NextFunction, Request, Response } from "express";
+import { Handler, NextFunction, Request, Response } from 'express'
 
 export default class Resolver {
-  handle(handlerFn: Handler) {
-    return (req: Request, res: Response, next: NextFunction) => {
-      return Promise.resolve(handlerFn(req, res, next)).catch((err) =>
+  handle (handlerFn: Handler) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      return await Promise.resolve(handlerFn(req, res, next)).catch((err) =>
         next(err)
-      );
-    };
+      )
+    }
   }
 }
