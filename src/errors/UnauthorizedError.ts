1e3cd18ddb999ea40b29179a9754b9a044aa48e1
joaov1ct0r <jov9051@outlook.com>
2023-04-26 20:23:25 -0300

    refactor: UnauthorizedError

--- src/errors/UnauthorizedError.ts
+++ src/errors/UnauthorizedError.ts
@@ -1,12 +1,10 @@
 export default class UnathorizedError extends Error {
-  public readonly message: string;
-  public readonly statusCode: number;
+  public readonly message: string
+  public readonly statusCode: number
 
-  constructor(message: string, statusCode = 401) {
-    super(message);
-
-    this.message = message;
-
-    this.statusCode = statusCode;
+  constructor (message: string, statusCode = 401) {
+    super(message)
+    this.message = message
+    this.statusCode = statusCode
   }
 }
