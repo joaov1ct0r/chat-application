d60ceac45b19708ff455273badc9b799f5081022
joaov1ct0r <jov9051@outlook.com>
2023-04-26 20:21:30 -0300

    refactor: userRoutes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,23 +1,25 @@
-import express from "express";
-import Resolver from "../utils/Resolver";
-import CreateUserController from "../controller/CreateUserController";
-import AuthenticateUserController from "../controller/AuthenticateUserController";
+import express from 'express'
+import Resolver from '../utils/Resolver'
+import CreateUserController from '../controller/CreateUserController'
+import AuthenticateUserController from '../controller/AuthenticateUserController'
 
-const userRouter: express.Router = express.Router();
+const userRouter: express.Router = express.Router()
 
-const createUserController: CreateUserController = new CreateUserController();
+const createUserController: CreateUserController = new CreateUserController()
 
 const authenticateUserController: AuthenticateUserController =
-  new AuthenticateUserController();
+  new AuthenticateUserController()
+
+const resolver: Resolver = new Resolver()
 
 userRouter.post(
-  "/login",
-  new Resolver().handle(authenticateUserController.handle)
-);
+  '/login',
+  resolver.handle(authenticateUserController.handle) // eslint-disable-line
+)
 
 userRouter.post(
-  "/register",
-  new Resolver().handle(createUserController.handle)
-);
+  '/register',
+  resolver.handle(createUserController.handle) // eslint-disable-line
+)
 
-export default userRouter;
+export default userRouter
