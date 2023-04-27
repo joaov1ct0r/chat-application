b74c65dc095bc1ecadfc92c7475494f038b6c7bd
joaov1ct0r <jov9051@outlook.com>
2023-04-27 19:59:17 -0300

    refactor: auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,43 +1,36 @@
-import { Response, NextFunction } from "express";
-import IReq from "../interfaces/IRequest";
-import jwt from "jsonwebtoken";
-import IJwt from "../interfaces/IJwt";
-import BadRequestError from "../errors/BadRequestError";
-import ForbiddenError from "../errors/ForbiddenError";
-import InternalError from "../errors/InternalError";
+import { Response, NextFunction } from 'express'
+import IReq from '../interfaces/IRequest'
+import jwt from 'jsonwebtoken'
+import IJwt from '../interfaces/IJwt'
+import BadRequestError from '../errors/BadRequestError'
+import ForbiddenError from '../errors/ForbiddenError'
 
 export default class Authorization {
-  handle(req: IReq, res: Response, next: NextFunction) {
-    const token: string = req.cookies.authorization.split(" ")[1];
+  handle (req: IReq, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined {
+    const token: string = req.cookies.authorization.split(' ')[1]
 
     if (token.length === 0) {
-      const error = new BadRequestError("Token não encontrado!");
+      const error = new BadRequestError('Token não encontrado!')
 
       return res
         .status(error.statusCode)
-        .json({ error: error.message, status: error.statusCode });
+        .json({ error: error.message, status: error.statusCode })
     }
 
     try {
       const verifiedToken: IJwt = jwt.verify(
         token,
         process.env.JWT_TOKEN_SECRET as string
-      ) as IJwt;
+      ) as IJwt
 
-      if (!verifiedToken) {
-        const error = new ForbiddenError("Falha na autenticação!");
-        return res
-          .status(error.statusCode)
-          .json({ error: error.message, status: error.statusCode });
-      }
-      req.userId = verifiedToken.id;
+      req.userId = verifiedToken.id
 
-      next();
+      next()
     } catch (err: unknown) {
-      const error = new InternalError("Erro Interno");
+      const error = new ForbiddenError('Falha na autenticação!')
       return res
         .status(error.statusCode)
-        .json({ error: error.message, status: error.statusCode });
+        .json({ error: error.message, status: error.statusCode })
     }
   }
 }
