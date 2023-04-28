0cea57e61455ba44db949b0ec08ca917f4ac37bc
joaov1ct0r <jov9051@outlook.com>
2023-04-28 19:24:40 -0300

    refactor: IAuthenticateUserRepository

--- src/interfaces/IAuthenticateUserRepository.ts
+++ src/interfaces/IAuthenticateUserRepository.ts
@@ -1,5 +1,5 @@
-import IUser from "./IUser";
+import IUser from './IUser'
 
 export default interface IAuthenticateUserRepository {
-  execute(email: string): Promise<IUser | null>;
+  execute: (email: string) => Promise<IUser | null>
 }
