a7a647e2b0ca48df80363e649647dd982008277e
joaov1ct0r <jov9051@outlook.com>
2023-04-28 19:22:07 -0300

    refactor: app

--- src/app.ts
+++ src/app.ts
@@ -35,13 +35,9 @@ export default class App {
         methods: ['GET', 'POST']
       })
     )
-
     this.server.use(cookieParser())
-
     this.server.use(express.json())
-
     this.server.use(express.urlencoded({ extended: true }))
-
     this.server.use(
       (
         error:
@@ -68,21 +64,18 @@ export default class App {
 
   private async routes (): Promise<void> {
     this.server.use('/api/user', userRouter)
-
     this.server.use(
       '/',
       process.env.NODE_ENV === 'production'
         ? express.static('build/views/login')
         : express.static('src/views/login')
     )
-
     this.server.use(
       '/register',
       process.env.NODE_ENV === 'production'
         ? express.static('build/views/registro')
         : express.static('src/views/registro')
     )
-
     this.server.use(
       '/chat',
       new Authorization().handle,
@@ -90,7 +83,6 @@ export default class App {
         ? express.static('build/views/chat')
         : express.static('src/views/chat')
     )
-
     this.server.use(
       '/api/docs',
       swaggerUi.serve,
