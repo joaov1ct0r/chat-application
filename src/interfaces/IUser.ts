883b9e272910167d7c2739551316bf68ea96bd59
joaov1ct0r <jov9051@outlook.com>
2023-04-28 19:26:54 -0300

    refactor: IUser

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,9 +1,9 @@
-import User from "../database/entities/User";
+import User from '../database/entities/User'
 
 export default interface IUser extends User {
-  id: number;
-  nome: string;
-  email: string;
-  nascimento: string;
-  senha: string;
+  id: number
+  nome: string
+  email: string
+  nascimento: string
+  senha: string
 }
