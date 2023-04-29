e7bd93bff02cbd854270e687c466a164f88c4b54
joaov1ct0r <jov9051@outlook.com>
2023-04-29 12:41:43 -0300

    refactor: CreateUserRepository

--- src/database/repositories/CreateUserRepository.ts
+++ src/database/repositories/CreateUserRepository.ts
@@ -1,18 +1,18 @@
-import { Repository } from "typeorm";
-import IUser from "../../interfaces/IUser";
-import DB from "../config/data-source";
-import User from "../entities/User";
-import ICreateUserRepository from "../../interfaces/ICreateUserRepository";
-import bcrypt from "bcryptjs";
+import { Repository } from 'typeorm'
+import IUser from '../../interfaces/IUser'
+import DB from '../config/data-source'
+import User from '../entities/User'
+import ICreateUserRepository from '../../interfaces/ICreateUserRepository'
+import bcrypt from 'bcryptjs'
 
 export default class CreateUserRepository implements ICreateUserRepository {
-  private readonly userRepository: Repository<IUser>;
+  private readonly userRepository: Repository<IUser>
 
-  constructor() {
-    this.userRepository = DB.getRepository(User);
+  constructor () {
+    this.userRepository = DB.getRepository(User)
   }
 
-  async execute(
+  async execute (
     email: string,
     nome: string,
     nascimento: string,
@@ -22,11 +22,10 @@ export default class CreateUserRepository implements ICreateUserRepository {
       email,
       nome,
       nascimento,
-      senha: bcrypt.hashSync(senha),
-    });
+      senha: bcrypt.hashSync(senha)
+    })
 
-    await this.userRepository.save(user);
-
-    return user;
+    await this.userRepository.save(user)
+    return user
   }
 }
