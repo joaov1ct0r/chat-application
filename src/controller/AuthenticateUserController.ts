21619a497c71d3cbc847705b3b0871fcde4a9dd1
joaov1ct0r <jov9051@outlook.com>
2023-04-30 19:43:03 -0300

    refactor: AuthenticateUserController

--- src/controller/AuthenticateUserController.ts
+++ src/controller/AuthenticateUserController.ts
@@ -1,52 +1,53 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
-import ValidateUser from "../validations/validateUserData";
-import AuthenticateUserService from "../services/AuthenticateUserService";
-import BadRequestError from "../errors/BadRequestError";
-import AuthenticateUserRepository from "../database/repositories/AuthenticateUserRepository";
+import { Request, Response } from 'express'
+import ValidateUser from '../validations/validateUserData'
+import AuthenticateUserService from '../services/AuthenticateUserService'
+import BadRequestError from '../errors/BadRequestError'
+import AuthenticateUserRepository from '../database/repositories/AuthenticateUserRepository'
 
 export default class AuthenticateUserController {
-  private readonly repository: AuthenticateUserRepository;
-  private readonly validateUser: ValidateUser;
-  private readonly authenticateUserService: AuthenticateUserService;
+  private readonly repository: AuthenticateUserRepository
+  private readonly validateUser: ValidateUser
+  private readonly authenticateUserService: AuthenticateUserService
 
-  constructor() {
-    this.repository = new AuthenticateUserRepository();
-    this.validateUser = new ValidateUser();
-    this.authenticateUserService = new AuthenticateUserService(this.repository);
+  constructor () {
+    this.repository = new AuthenticateUserRepository()
+    this.validateUser = new ValidateUser()
+    this.authenticateUserService = new AuthenticateUserService(this.repository)
   }
-  public async handle(
+
+  public async handle (
     req: Request,
     res: Response
-  ): Promise<void | Response> {
-    const { error } = this.validateUser.loginValidate(req.body);
+  ): Promise<Response> {
+    const { error } = this.validateUser.loginValidate(req.body)
 
-    if (error) {
-      const err = new BadRequestError(error.message);
+    if (error != null) {
+      const err = new BadRequestError(error.message)
 
-      return res.status(err.statusCode).json({ error, status: err.statusCode });
+      return res.status(err.statusCode).json({ error, status: err.statusCode })
     }
 
-    const email: string = req.body.email;
+    const email: string = req.body.email
 
-    const senha: string = req.body.senha;
+    const senha: string = req.body.senha
 
     try {
-      const token: string = await this.authenticateUserService.execute(email, senha);
+      const token: string = await this.authenticateUserService.execute(email, senha)
 
-      res.cookie("authorization", `Bearer ${token}`, {
+      res.cookie('authorization', `Bearer ${token}`, {
         httpOnly: true,
-        path: "/chat",
-      });
+        path: '/chat'
+      })
 
       return res
         .status(200)
-        .json({ message: "Login realizado com sucesso!", status: 200 });
+        .json({ message: 'Login realizado com sucesso!', status: 200 })
     } catch (err: any) {
       return res.status(err.statusCode).json({
         error: err.message,
-        status: err.statusCode,
-      });
+        status: err.statusCode
+      })
     }
   }
 }
