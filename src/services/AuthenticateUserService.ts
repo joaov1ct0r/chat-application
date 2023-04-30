19f71a0760916306d7799f80dbdf676f4092f482
joaov1ct0r <jov9051@outlook.com>
2023-04-30 19:41:18 -0300

    refactor: AuthenticateUserService

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,39 +1,39 @@
-import "dotenv/config";
-import IUser from "../interfaces/IUser";
-import BadRequestError from "../errors/BadRequestError";
-import UnathorizedError from "../errors/UnauthorizedError";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import IAuthenticateUserRepository from "../interfaces/IAuthenticateUserRepository";
+import 'dotenv/config'
+import IUser from '../interfaces/IUser'
+import BadRequestError from '../errors/BadRequestError'
+import UnathorizedError from '../errors/UnauthorizedError'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import IAuthenticateUserRepository from '../interfaces/IAuthenticateUserRepository'
 
 export default class AuthenticateUserService {
-  private readonly repository: IAuthenticateUserRepository;
+  private readonly repository: IAuthenticateUserRepository
 
-  constructor(repository: IAuthenticateUserRepository) {
-    this.repository = repository;
+  constructor (repository: IAuthenticateUserRepository) {
+    this.repository = repository
   }
 
-  public async execute(email: string, senha: string): Promise<string> {
-    const user: IUser | null = await this.repository.execute(email);
+  public async execute (email: string, senha: string): Promise<string> {
+    const user: IUser | null = await this.repository.execute(email)
 
     if (user === null) {
-      throw new BadRequestError("Usuario não encontrado!");
+      throw new BadRequestError('Usuario não encontrado!')
     }
 
-    const comparedPassword: boolean = bcrypt.compareSync(senha, user.senha);
+    const comparedPassword: boolean = bcrypt.compareSync(senha, user.senha)
 
-    if (comparedPassword === false) {
-      throw new UnathorizedError("Falha na autenticação!");
+    if (!comparedPassword) {
+      throw new UnathorizedError('Falha na autenticação!')
     }
 
     const token: string = jwt.sign(
       {
-        id: user.id,
+        id: user.id
       },
       process.env.JWT_TOKEN_SECRET as string,
       { expiresIn: 300 }
-    );
+    )
 
-    return token;
+    return token
   }
 }
