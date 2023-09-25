c9680ae206ef39998f14bb86f1b18feccb5a82d0
joaov1ct0r <jov9051@outlook.com>
2023-09-24 21:36:37 -0300

    refactor: data-source

--- src/database/config/data-source.ts
+++ src/database/config/data-source.ts
@@ -1,28 +1,19 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import 'dotenv/config'
 import { DataSource } from 'typeorm'
 import User from '../entities/User'
 
-const DB: DataSource = new DataSource({
+const databaseClient: DataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT!),
-  username: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  database:
-    process.env.NODE_ENV! === 'production'
-      ? process.env.DB_DATABASE!
-      : process.env.DB_DATABASE_TEST!,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   logging: true,
   entities: [User],
   subscribers: [],
-  migrations: [
-    process.env.NODE_ENV! === 'production'
-      ? 'build/migrations/**'
-      : 'src/migrations/**'
-  ],
+  migrations: ['src/migrations/**'],
   migrationsRun: true
 })
 
-export default DB
+export default databaseClient
