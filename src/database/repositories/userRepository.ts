8a357131b3b6d34d344c198da01a18108bb3cede
joaov1ct0r <jov9051@outlook.com>
2023-09-25 21:04:27 -0300

    refactor: UserRepository

--- src/database/repositories/userRepository.ts
+++ src/database/repositories/userRepository.ts
@@ -1,5 +1,4 @@
+import User from '@Database/entities/User'
 import { BaseRepository } from '@Database/repositories/baseRepository'
 
-export default class UserRepository<
-  S extends object,
-> extends BaseRepository<S> {}
+export default class UserRepository<S extends User> extends BaseRepository<S> {}
