3c0b0c2655d4a1576204f608ee233fc9a004c020
joaov1ct0r <jov9051@outlook.com>
2023-09-27 20:17:21 -0300

    feat: userFactory

--- src/factories/userFactory.ts
+++ src/factories/userFactory.ts
@@ -1,6 +1,8 @@
+import AuthenticateUserController from '@Controllers/AuthenticateUserController'
 import CreateUserController from '@Controllers/CreateUserController'
 import User from '@Database/entities/User'
 import UserRepository from '@Database/repositories/userRepository'
+import AuthenticateUserService from '@Services/AuthenticateUserService'
 import CreateUserService from '@Services/CreateUserService'
 import Validator from '@Validators/validator'
 
@@ -10,6 +12,11 @@ export default class UserFactory {
       case 'create':
         return new CreateUserController(
           new CreateUserService(new UserRepository(User)),
+          new Validator(),
+        )
+      case 'authenticate':
+        return new AuthenticateUserController(
+          new AuthenticateUserService(new UserRepository(User)),
           new Validator(),
         )
       default:
